2c4220a3b9c6da80361045457a34213f528d26ee
Sean Kozak <srkozak2@eceb-2022-14.ews.illinois.edu>
2017-03-12 13:30:02 -0500

    add mem_wb stage reg

--- mp3/mem_stage.sv
+++ mp3/mem_stage.sv
@@ -2,14 +2,16 @@ import lc3b_types::*;
 
 module mem_stage
 (
+	input clk,
 	input EX_MEM ex_mem,
 	input lc3b_word mem_rdata,
 	input mdr_mux_sel,
 	input marmux_sel,
-	output MEM_WB mem_wb,
+	output MEM_WB mem_wb_out,
 	output lc3b_word mem_wdata, address
 );
 
+MEM_WB mem_wb_in;
 
 mux2 mar_mux
 (
@@ -27,11 +29,17 @@ mux2 mdr_mux
 	.f(mem_wdata)
 );
 
-assign mem_address = ex_mem.alu_out;
 //assign mem_wb.address = ex_mem.address;
-assign mem_wb.mem_data = mem_wdata;
-assign mem_wb.pc_out = ex_mem.pc_out;
-assign mem_wb.alu_out = ex_mem.alu_out;
-assign mem_wb.intr = ex_mem.intr;
+assign mem_wb_in.mem_data = mem_wdata;
+assign mem_wb_in.pc_out = ex_mem.pc_out;
+assign mem_wb_in.alu_out = ex_mem.alu_out;
+assign mem_wb_in.intr = ex_mem.intr;
+
+register #(.width($bits(MEM_WB))) mem_wb
+ (
+	.clk,
+	.in(mem_wb_in),
+	.out(mem_wb_out)
+ );
 
 endmodule : mem_stage
