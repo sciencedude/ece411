29079155ebf9ec6a32c76e13fa7f163ed5849100
spatri5 <spatri5@illinois.edu>
2017-03-11 20:55:41 -0600

    Replace execute.sv forgot to save changes here is the final submission

--- mp3/execute.sv
+++ mp3/execute.sv
@@ -10,6 +10,9 @@ module execute
 EX_MEM ex_mem_in;
 lc3b_word srcamux_out;
 lc3b_word srcbmux_out;
+lc3b_word imm5_out;
+lc3b_word adj6_out;
+lc3b_word adj9_out;
 mux2 srcamux
 (
 	.sel(id_ex_out.control_signals.srcamux_sel),
@@ -17,13 +20,33 @@ mux2 srcamux
 	.b(id_ex_out.pc_out),
 	.f(srcamux_out)
 );
+
+sext#(.width(5)) imm5
+(
+	.in(id_ex_out.intr[4:0]),
+	.out(imm5_out)
+);
+
+adj #(.width(9)) adj9
+(
+	.in(id_ex_out.intr[8:0]),
+	.out(adj9_out)
+);
+
+adj #(.width(6)) adj6
+(
+	.in(id_ex_out.intr[5:0]),
+	.out(adj6_out)
+);
+
+
 mux4 srcbmux
 (
 	.sel(id_ex_out.control_signals.srcbmux_sel),
 	.a(id_ex_out.srcb_out),
-	.b(),
-	.c(),
-	.d(),
+	.b(imm5_out),
+	.c(adj6_out),
+	.d(adj9_out),
 	.f(srcbmux_out)
 );
 
